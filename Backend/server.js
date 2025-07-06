deca50bae03e8c0fa1144f2171780be087a7e900
shital2809 <shitalkendre049@gmail.com>
2025-07-06 16:36:54 +0530

    initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,35 @@ dotenv.config();
 const app = express();
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "https://vendor-user.netlify.app",
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "https://super-admin-ga55.onrender.com",
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// CORS headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();
+});
 app.use(express.json());
 app.use("/api/user", authRoutes);
 app.use("/api/vendor", vendorRoutes);
